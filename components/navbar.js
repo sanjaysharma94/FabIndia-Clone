cd931226f638d0d17127b806ff1a9ab53733b917
Arjun-9696 <mallikarjunharalalli@gmail.com>
2022-01-22 03:47:49 +0530

    Navbar
    Components/navbar.js

---
+++ components/navbar.js
@@ -0,0 +1,192 @@
+function navbar (){
+return `<div id="header">
+      <div id="navbar">
+        <div id="nav-left">
+            <div>
+                 <a href=""><img class="iconimg" src="https://www.fabindia.com/file/general/icon-storelocator-black.png" alt="Store Locator"/></a>
+            </div>
+            <div>
+                <a href=""><img class="iconimg" src="https://www.fabindia.com/file/general/icon-signin-black.png" alt="Store Locator"/></a>
+            </div>
+       
+        </div>
+        <div id="logo">
+            <a href=""><img class="logo" src="https://www.fabindia.com/file/general/Fabindia_logo.svg" alt="Store Locator"/></a>
+        </div>
+        <div id="nav-right">
+            <div id="nav-right-sub">
+            <div>
+                <a href="#">
+                <img id="india" src="https://www.fabindia.com/file/v8385693196996654632/general/india_flag.svg" alt="india" title="india">
+            </a>
+            </div>
+            <div>
+                <a href=""><img class="iconimg" src="https://www.fabindia.com/file/general/icon-search-black.svg" alt="Store Locator"/></a>
+            </div>
+            <div>
+                <a href=""><img class="iconimg" src="https://www.fabindia.com/file/general/icon-heart-black.png" alt="Store Locator"/></a>
+            </div>
+            <div>
+                <a href=""><img class="iconimg" src="https://www.fabindia.com/file/general/icon-bag-black.png" alt="Store Locator"/></a>
+            </div>
+        </div>
+        </div>
+      </div>
+      <div id="cat">
+      <div class="subnavbar">
+          <button class="subnavbtn">
+            NEW ARRIVALS
+            <i class="cart_down"></i>
+          </button>
+          <div class="subnav-content">
+              <div>
+            <a href="#company">Fashion</a>
+            <a href="#company">Men</a>
+            <a href="#company">Women</a>
+            <a href="#company">Electronics</a>
+            <a href="her_per.html">Beauty & Grooming</a>
+            <a href="#company">Home & Kitchen</a>
+              </div>
+            
+          </div>
+        </div>
+        <div class="subnavbar">
+          <button class="subnavbtn">
+            COLLECTIONS
+            <i class="cart_down"></i>
+          </button>
+          <div class="subnav-content">
+            <a href="#company">Fashion</a>
+            <a href="#company">Men</a>
+            <a href="#company">Women</a>
+            <a href="#company">Electronics</a>
+            <a href="her_per.html">Beauty & Grooming</a>
+            <a href="#company">Home & Kitchen</a>
+          </div>
+        </div>
+        <div class="subnavbar">
+          <button class="subnavbtn">
+            WOMEN
+            <i class="cart_down"></i>
+          </button>
+          <div class="subnav-content">
+            <a href="#company">Fashion</a>
+            <a href="#company">Men</a>
+            <a href="#company">Women</a>
+            <a href="#company">Electronics</a>
+            <a href="her_per.html">Beauty & Grooming</a>
+            <a href="#company">Home & Kitchen</a>
+          </div>
+        </div>
+        <div class="subnavbar">
+          <button class="subnavbtn">
+            MEN
+            <i class="cart_down"></i>
+          </button>
+          <div class="subnav-content">
+            <a href="#company">Fashion</a>
+            <a href="#company">Men</a>
+            <a href="#company">Women</a>
+            <a href="#company">Electronics</a>
+            <a href="her_per.html">Beauty & Grooming</a>
+            <a href="#company">Home & Kitchen</a>
+          </div>
+        </div>
+        <div class="subnavbar">
+          <button class="subnavbtn">
+            KIDS
+            <i class="cart_down"></i>
+          </button>
+          <div class="subnav-content">
+            <a href="#company">Fashion</a>
+            <a href="#company">Men</a>
+            <a href="#company">Women</a>
+            <a href="#company">Electronics</a>
+            <a href="her_per.html">Beauty & Grooming</a>
+            <a href="#company">Home & Kitchen</a>
+          </div>
+        </div>
+        <div class="subnavbar">
+          <button class="subnavbtn">
+            HOME LINEN
+            <i class="cart_down"></i>
+          </button>
+          <div class="subnav-content">
+            <a href="#company">Fashion</a>
+            <a href="#company">Men</a>
+            <a href="#company">Women</a>
+            <a href="#company">Electronics</a>
+            <a href="her_per.html">Beauty & Grooming</a>
+            <a href="#company">Home & Kitchen</a>
+          </div>
+        </div>
+        <div class="subnavbar">
+          <button class="subnavbtn">
+            DINING & DECOR
+            <i class="cart_down"></i>
+          </button>
+          <div class="subnav-content">
+            <a href="#company">Fashion</a>
+            <a href="#company">Men</a>
+            <a href="#company">Women</a>
+            <a href="#company">Electronics</a>
+            <a href="her_per.html">Beauty & Grooming</a>
+            <a href="#company">Home & Kitchen</a>
+          </div>
+        </div>
+        <div class="subnavbar">
+          <button class="subnavbtn">
+            FURNITURE
+            <i class="cart_down"></i>
+          </button>
+          <div class="subnav-content">
+            <a href="#company">Fashion</a>
+            <a href="#company">Men</a>
+            <a href="#company">Women</a>
+            <a href="#company">Electronics</a>
+            <a href="her_per.html">Beauty & Grooming</a>
+            <a href="#company">Home & Kitchen</a>
+          </div>
+        </div>
+        <div class="subnavbar">
+          <button class="subnavbtn">
+            BEAUTY
+            <i class="cart_down"></i>
+          </button>
+          <div class="subnav-content">
+            <a href="#company">Fashion</a>
+            <a href="#company">Men</a>
+            <a href="#company">Women</a>
+            <a href="#company">Electronics</a>
+            <a href="her_per.html">Beauty & Grooming</a>
+            <a href="#company">Home & Kitchen</a>
+          </div>
+        </div>
+        <div class="subnavbar">
+          <button class="subnavbtn">
+            FOOD
+            <i class="cart_down"></i>
+          </button>
+          <div class="subnav-content">
+            <a href="#company">Fashion</a>
+            <a href="#company">Men</a>
+            <a href="#company">Women</a>
+            <a href="#company">Electronics</a>
+            <a href="her_per.html">Beauty & Grooming</a>
+            <a href="#company">Home & Kitchen</a>
+          </div>
+        </div>
+        <div class="subnavbar">
+          <button class="subnavbtn" id="custom">
+            CUSTOM KURTAS!
+          </button>
+        </div>
+        <div class="subnavbar">
+          <button class="subnavbtn" id="sale">
+            SALE!
+          </button>
+        </div>
+        </div>
+    </div>`;
+}
+export default navbar
